fca130ad9371b802fd8d79e0eeb56848e017b354
G Sai Vikrant <saivikrant.cs21@bmsce.ac.in>
2024-11-27 20:03:31 +0530

    Implemented Fill.asm

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,57 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(LOOP)
+    @KBD
+    D = M
+    @DISPLAY
+    D;JNE
+    @WHITE
+    0;JMP
+
+(CONT1)
+    D = 0
+    @LOOP
+    0;JMP
+
+(DISPLAY)
+    @BLACK
+    0;JMP
+
+(BLACK)
+    @cursor
+    M = 0
+(COVER)
+    @cursor
+    D = M
+    @SCREEN
+    A = A+D
+    M = -1
+    D = D+1
+    @cursor
+    M = D
+    @8192
+    D = D-A
+    @COVER
+    D;JLT
+    @CONT1
+    0;JMP
+
+(WHITE)
+    @cursor
+    M = 0
+(COVER1)
+    @cursor
+    D = M
+  @SCREEN
+    A = A+D
+    M = 0
+    D = D+1
+    @cursor
+    M = D
+    @8192
+    D = D-A
+    @COVER1
+    D;JLT
+    @CONT1
+    0;JMP
